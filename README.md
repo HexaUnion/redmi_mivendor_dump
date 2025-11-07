82ff38e55740e99809355a75ee9eca19f7ab4989
Hexaboo <hexaboo@outlook.com>
2025-11-07 11:02:12 +0700

    Add extras for missi-user 15 AQ3A.250226.002 OS2.0.210.0.VBOMIXM release-keys
    Signed-off-by: Hexaboo <hexaboo@outlook.com>

---
+++ README.md
@@ -0,0 +1,20 @@
+## missi-user 15 AQ3A.250226.002 OS2.0.210.0.VBOMIXM release-keys
+- Manufacturer: xiaomi
+- Platform: bengal
+- Codename: mivendor
+- Brand: Redmi
+- Flavor: missi-user
+- Release Version: 15
+- Kernel Version: 5.15.167
+- Id: AQ3A.250226.002
+- Incremental: OS2.0.210.0.VBOMIXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Treble Device: true
+- Locale: en-GB
+- Screen Density: undefined
+- Fingerprint: Redmi/sm6225/mivendor:13/TKQ1.250214.001/OS2.0.210.0.VBOMIXM:user/release-keys
+- OTA version: 
+- Branch: missi-user-15-AQ3A.250226.002-OS2.0.210.0.VBOMIXM-release-keys
+- Repo: redmi_mivendor_dump
---
+++ twrp-device-tree/xiaomi/creek/omni_creek.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from creek device
+$(call inherit-product, device/xiaomi/creek/device.mk)
+
+PRODUCT_DEVICE := creek
+PRODUCT_NAME := omni_creek
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := creek
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="creek_global-user 13 TKQ1.250214.001 OS2.0.210.0.VBOMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/creek_global/creek:13/TKQ1.250214.001/OS2.0.210.0.VBOMIXM:user/release-keys
